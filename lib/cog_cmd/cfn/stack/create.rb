26208b3cafd0e331a0018e406ea13615cb89ee66
Matthew Peck <matt@operable.io>
2016-08-25 16:05:14 -0400

    Don't nest subcommand classes

--- lib/cog_cmd/cfn/stack/create.rb
+++ lib/cog_cmd/cfn/stack/create.rb
@@ -1,51 +1,49 @@
 require_relative '../exceptions'
 require_relative '../helpers'
 
-module CogCmd::Cfn
-  class Stack < Cog::AggregateCommand
-    class Create < Cog::SubCommand
+class CogCmd::Cfn::Stack::Create < Cog::SubCommand
 
-      include CogCmd::Cfn::Helpers
+  include CogCmd::Cfn::Helpers
 
-      USAGE = <<-END.gsub(/^ {5}/, '')
-      Usage: cfn:stack create <stack name> <template name> [options]
+  USAGE = <<-END.gsub(/^ {2}/, '')
+  Usage: cfn:stack create <stack name> <template name> [options]
 
-      Creates a new stack based on an existing template
+  Creates a new stack based on an existing template
 
-      Options:
-        --param, -p "Key1=Value1"                   (Can be specified multiple times)
-        --tag, -t "Name1=Value1"                    (Can be specified multiple times)
-        --policy, -o "s3_policy_name"
-        --notify, -n "NotifyArn"                    (Can be specified multiple times)
-        --on-failure, -f <rollback | delete | keep>
-        --timeout, -e <minutes>
-        --capabilities, -c <iam | named_iam>
-      END
+  Options:
+    --param, -p "Key1=Value1"                   (Can be specified multiple times)
+    --tag, -t "Name1=Value1"                    (Can be specified multiple times)
+    --policy, -o "s3_policy_name"
+    --notify, -n "NotifyArn"                    (Can be specified multiple times)
+    --on-failure, -f <rollback | delete | keep>
+    --timeout, -e <minutes>
+    --capabilities, -c <iam | named_iam>
 
-      def run
-        unless request.args.length >= 2
-          raise CogCmd::Cfn::ArgumentError, "You must specify a stack name AND a template name."
-        end
+  Returns the newly created stack.
+  END
 
-        cloudform = Aws::CloudFormation::Client.new()
-        cf_params = Hash[
-          [
-            [ :stack_name, request.args[0] ],
-            [ :template_url, template_url(request.args[1]) ],
-            param_or_nil([ :parameters, process_parameters(request.options["param"]) ]),
-            param_or_nil([ :tags, process_tags(request.options["tag"]) ]),
-            param_or_nil([ :stack_policy_url, policy_url(request.options["policy"]) ]),
-            param_or_nil([ :notification_arns, request.options["notify"] ]),
-            param_or_nil([ :on_failure, process_on_failure(request.options["on-failure"]) ]),
-            param_or_nil([ :timeout_in_minutes, request.options["timeout"] ]),
-            param_or_nil([ :capabilities, process_capabilities(request.options["capabilities"]) ])
-          ].compact
-        ]
-
-        cloudform.create_stack(cf_params)
-        cloudform.describe_stacks(stack_name: request.args[0]).stacks[0].to_h
-      end
-
+  def run
+    unless request.args.length >= 2
+      raise CogCmd::Cfn::ArgumentError, "You must specify a stack name AND a template name."
     end
+
+    cloudform = Aws::CloudFormation::Client.new()
+    cf_params = Hash[
+      [
+        [ :stack_name, request.args[0] ],
+        [ :template_url, template_url(request.args[1]) ],
+        param_or_nil([ :parameters, process_parameters(request.options["param"]) ]),
+        param_or_nil([ :tags, process_tags(request.options["tag"]) ]),
+        param_or_nil([ :stack_policy_url, policy_url(request.options["policy"]) ]),
+        param_or_nil([ :notification_arns, request.options["notify"] ]),
+        param_or_nil([ :on_failure, process_on_failure(request.options["on-failure"]) ]),
+        param_or_nil([ :timeout_in_minutes, request.options["timeout"] ]),
+        param_or_nil([ :capabilities, process_capabilities(request.options["capabilities"]) ])
+      ].compact
+    ]
+
+    cloudform.create_stack(cf_params)
+    cloudform.describe_stacks(stack_name: request.args[0]).stacks[0].to_h
   end
+
 end
--- lib/cog_cmd/cfn/stack/list.rb
+++ lib/cog_cmd/cfn/stack/list.rb
@@ -1,57 +1,55 @@
 require_relative '../helpers'
 
-module CogCmd::Cfn
-  class Stack < Cog::AggregateCommand
-    class List < Cog::SubCommand
+class CogCmd::Cfn::Stack::List < Cog::SubCommand
 
-      include CogCmd::Cfn::Helpers
+  include CogCmd::Cfn::Helpers
 
-      USAGE = <<-END.gsub(/^ {5}/, '')
-      Usage: cfn:stack list [options]
+  USAGE = <<-END.gsub(/^ {2}/, '')
+  Usage: cfn:stack list [options]
 
-      Options:
-        --filter "status filter"    (Can be specified multiple times) (Defaults to 'ACTIVE')
+  Options:
+    --filter "status filter"    (Can be specified multiple times) (Defaults to 'ACTIVE')
 
-      Notes:
-        The filter string can be one or more cloudformation stack status strings which include:
-        CREATE_IN_PROGRESS, CREATE_FAILED, CREATE_COMPLETE, ROLLBACK_IN_PROGRESS, ROLLBACK_FAILED, ROLLBACK_COMPLETE, DELETE_IN_PROGRESS, DELETE_FAILED, DELETE_COMPLETE, UPDATE_IN_PROGRESS, UPDATE_COMPLETE_CLEANUP_IN_PROGRESS, UPDATE_COMPLETE, UPDATE_ROLLBACK_IN_PROGRESS, UPDATE_ROLLBACK_FAILED, UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS, UPDATE_ROLLBACK_COMPLETE
+  Notes:
+    The filter string can be one or more cloudformation stack status strings which include:
+    CREATE_IN_PROGRESS, CREATE_FAILED, CREATE_COMPLETE, ROLLBACK_IN_PROGRESS, ROLLBACK_FAILED, ROLLBACK_COMPLETE, DELETE_IN_PROGRESS, DELETE_FAILED, DELETE_COMPLETE, UPDATE_IN_PROGRESS, UPDATE_COMPLETE_CLEANUP_IN_PROGRESS, UPDATE_COMPLETE, UPDATE_ROLLBACK_IN_PROGRESS, UPDATE_ROLLBACK_FAILED, UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS, UPDATE_ROLLBACK_COMPLETE
 
-        Additionally a few special filter strings that correspond to a subset of the standard filter strings may be used:
-        ACTIVE, COMPLETE, FAILED, DELETED, IN_PROGRESS
-      END
+    Additionally a few special filter strings that correspond to a subset of the standard filter strings may be used:
+    ACTIVE, COMPLETE, FAILED, DELETED, IN_PROGRESS
 
-      SPECIAL_FILTERS = %w(ACTIVE COMPLETE FAILED DELETED IN_PROGRESS)
+  Returns a list of stack summaries.
+  END
 
-      ACTIVE = %w(CREATE_IN_PROGRESS CREATE_COMPLETE ROLLBACK_IN_PROGRESS ROLLBACK_COMPLETE UPDATE_IN_PROGRESS UPDATE_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_COMPLETE UPDATE_ROLLBACK_IN_PROGRESS UPDATE_ROLLBACK_FAILED UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE)
-      COMPLETE = %w(CREATE_COMPLETE ROLLBACK_COMPLETE DELETE_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE)
-      FAILED = %w(CREATE_FAILED ROLLBACK_FAILED DELETE_FAILED UPDATE_ROLLBACK_FAILED)
-      DELETED = %w(DELETE_IN_PROGRESS DELETE_COMPLETE)
-      IN_PROGRESS = %w(CREATE_IN_PROGRESS ROLLBACK_IN_PROGRESS DELETE_IN_PROGRESS UPDATE_IN_PROGRESS UPDATE_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_ROLLBACK_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS)
+  SPECIAL_FILTERS = %w(ACTIVE COMPLETE FAILED DELETED IN_PROGRESS)
 
-      def run
-        cloudform = Aws::CloudFormation::Client.new()
-        cf_params = {}
+  ACTIVE = %w(CREATE_IN_PROGRESS CREATE_COMPLETE ROLLBACK_IN_PROGRESS ROLLBACK_COMPLETE UPDATE_IN_PROGRESS UPDATE_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_COMPLETE UPDATE_ROLLBACK_IN_PROGRESS UPDATE_ROLLBACK_FAILED UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE)
+  COMPLETE = %w(CREATE_COMPLETE ROLLBACK_COMPLETE DELETE_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE)
+  FAILED = %w(CREATE_FAILED ROLLBACK_FAILED DELETE_FAILED UPDATE_ROLLBACK_FAILED)
+  DELETED = %w(DELETE_IN_PROGRESS DELETE_COMPLETE)
+  IN_PROGRESS = %w(CREATE_IN_PROGRESS ROLLBACK_IN_PROGRESS DELETE_IN_PROGRESS UPDATE_IN_PROGRESS UPDATE_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_ROLLBACK_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS)
 
-        filters = request.options['filter'] || ['ACTIVE']
-        cf_params[:stack_status_filter] = process_filters(filters)
+  def run
+    cloudform = Aws::CloudFormation::Client.new()
+    cf_params = {}
 
-        stack_summaries = cloudform.list_stacks(cf_params).stack_summaries
+    filters = request.options['filter'] || ['ACTIVE']
+    cf_params[:stack_status_filter] = process_filters(filters)
 
-        stack_summaries.map(&:to_h)
+    stack_summaries = cloudform.list_stacks(cf_params).stack_summaries
+
+    stack_summaries.map(&:to_h)
+  end
+
+  private
+
+  def process_filters(filters)
+    filters.map do |filter|
+      filter = filter.upcase
+      if SPECIAL_FILTERS.include?(filter)
+        self.class.const_get(filter)
+      else
+        filter
       end
-
-      private
-
-      def process_filters(filters)
-        filters.map do |filter|
-          filter = filter.upcase
-          if SPECIAL_FILTERS.include?(filter)
-            self.class.const_get(filter)
-          else
-            filter
-          end
-        end.flatten.uniq
-      end
-    end
+    end.flatten.uniq
   end
 end
