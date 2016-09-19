a2d103e7ee5634950fafac2c2b712c935ec61a3f
Matthew Peck <matt@operable.io>
2016-09-19 17:18:30 -0400

    changeset-delete

--- lib/cog_cmd/cfn/changeset/delete.rb
+++ lib/cog_cmd/cfn/changeset/delete.rb
@@ -1,32 +1,37 @@
-require_relative '../exceptions'
+require 'cog_cmd/cfn/helpers'
 
-class CogCmd::Cfn::Changeset::Delete < Cog::Command
+module CogCmd::Cfn::Changeset
+  class Delete < Cog::Command
 
-  USAGE = <<~END
-  Usage: cfn:changeset delete <change set id> | <change set name> <stack name>
+  attr_reader :changeset_name_or_id, :stack_name
 
-  Deletes a changeset. Returns a map with containing the change set name AND stack name OR the change set id, depending on which was provided to apply.
-
-  Note: This command returns the same regardless of success or failure. Use the 'cfn:stack event' command to view the results of the delete.
-  END
+  def initialize
+    @changeset_name_or_id = request.args[0]
+    @stack_name = request.args[1]
+  end
 
   def run_command
-    unless request.args[0]
-      raise CogCmd::Cfn::ArgumentError, "You must specify either the change set id OR the change set name AND stack name."
-    end
+    raise(Cog::Error, "You must specify either the change set id OR the change set name AND stack name.") unless changeset_name_or_id
 
+    response.template = 'changeset_delete'
+    response.content = delete_changeset
+  end
+
+  private
+
+  def delete_changeset
     client = Aws::CloudFormation::Client.new()
 
-    cs_params = { change_set_name: request.args[0] }
-    if stack_name = request.args[1]
-      cs_params[:stack_name] = stack_name
-    end
+    cs_params = {
+      change_set_name: changeset_name_or_id,
+      stack_name: stack_name
+    }.reject { |_key, value| value.nil? }
 
     client.delete_change_set(cs_params)
 
-    { status: "delete initiated",
-      change_set_name_or_id: request.args[0],
-      stack_name: request.args[1] }
+    { changeset_name_or_id: changeset_name_or_id,
+      stack_name: stack_name }
   end
 
+  end
 end
