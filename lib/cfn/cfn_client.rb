4ce0968b46de9d40ecaaa19ddfcafda3b4a7045d
vanstee <patrick@vanstee.me>
2016-11-17 22:37:56 -0500

    Validate templates before creating a definition

---
+++ lib/cfn/cfn_client.rb
@@ -0,0 +1,25 @@
+require 'aws-sdk'
+require 'time'
+
+module Cfn
+  class CfnClient
+    def initialize(aws_sts_role_arn = nil)
+      update_aws_credentials(aws_sts_role_arn) if aws_sts_role_arn
+
+      @client = Aws::CloudFormation::Client.new
+    end
+
+    def validate_template(template_body)
+      @client.validate_template(template_body: template_body)
+    end
+
+    def update_aws_credentials(aws_sts_role_arn)
+      Aws.config.update(
+        credentials: Aws::AssumeRoleCredentials.new(
+          role_arn: aws_sts_role_arn,
+          role_session_name: "cog-#{ENV['COG_USER']}"
+        )
+      )
+    end
+  end
+end
