9b6527893791d3277f39de22e7e62c36c9c81ed3
Mark Imbriaco <mark@imbriaco.com>
2016-11-29 17:01:54 -0500

    add pagination for events/resources and tweak templates

--- lib/cog_cmd/cfn/stack/events.rb
+++ lib/cog_cmd/cfn/stack/events.rb
@@ -6,7 +6,9 @@ module CogCmd::Cfn::Stack
 
     include CogCmd::Cfn::Helpers
 
-  attr_reader :stack_name
+    PAGE_SIZE=10
+
+    attr_reader :stack_name
 
     def initialize
       # args
@@ -16,15 +18,30 @@ module CogCmd::Cfn::Stack
     def run_command
       raise(Cog::Abort, "You must specify a stack name.") unless stack_name
 
+      page = request.options["page"].nil? ? 1 : request.options["page"].to_i
+      events = describe_events(page) || raise(Cog::Abort, "#{self.name}: No events found for #{@stack_name} page #{page}")
+
+      events[0]["meta"] = {
+        stack_name: @stack_name,
+        page: events.size < PAGE_SIZE ? "#{page} (Last)" : page
+      }
+
       response.template = 'stack_event_list'
-      response.content = describe_events
+      response.content = events
     end
 
     private
 
-    def describe_events
-      client = Aws::CloudFormation::Client.new()
-      client.describe_stack_events(stack_name: stack_name).stack_events.map(&:to_h)
+    def describe_events(page)
+      page = 1 if page.nil? || page < 1
+      first_item = (page - 1) * PAGE_SIZE
+      last_item = (first_item + PAGE_SIZE) - 1
+
+      client = Aws::CloudFormation::Client.new
+
+      result = client.describe_stack_events(stack_name: stack_name).stack_events
+      page_results = result[first_item .. last_item]
+      page_results.nil? ? nil : page_results.map(&:to_h)
     end
   end
 end
